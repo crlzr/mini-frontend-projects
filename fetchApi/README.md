2be0e2ee458a2303a3c1808c23aaeaabf6452818
Carole <152753204+crlzr@users.noreply.github.com>
2025-12-30 09:21:03 +1100

    fetchApi task

---
+++ fetchApi/README.md
@@ -0,0 +1,21 @@
+## API to Use
+
+Use the public JSONPlaceholder API:
+
+Endpoint: https://jsonplaceholder.typicode.com/users
+
+This returns a list of fake users in JSON format.
+
+## Requirements
+
+- Create a simple HTML page.
+- Use fetch() to request the users from the API.
+- Convert the response to JSON.
+
+#### Display each user's:
+
+- Name
+- Email
+- City
+
+Handle loading and error states.
---
+++ fetchApi/script.js
@@ -0,0 +1,24 @@
+const users = document.querySelector(".users")
+const ulList = document.querySelector(".ulList")
+
+async function getUsers() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users")
+    if (!response.ok) {
+        throw new Error("not ok");
+    }
+    let data = await response.json()
+    if (!data) {
+        throw new Error("no Data")
+    }
+
+    for (person of data) {
+        const listElement = document.createElement("li");
+        listElement.textContent = `
+        Name: ${person.name} Email: ${person.email}`
+        ulList.append(listElement)
+
+    }
+    console.log(data);
+}
+
+getUsers();
